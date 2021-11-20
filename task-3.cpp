6b45ea379cba3f817d40b18687f2a11428eeb22a
AbhayRautela1 <87599266+AbhayRautela1@users.noreply.github.com>
2021-11-20 09:53:30 +0530

    Add files via upload

---
+++ task-3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+class Human{
+    public:
+        static int count;
+        Human()
+        {
+            count++;
+        }
+};
+int Human::count=0;
+
+
+int main()
+{
+    Human anil;
+    Human a;
+    Human l;
+    Human m;
+    cout<<"Count = "<<Human::count;
+
+    return 0;
+}
